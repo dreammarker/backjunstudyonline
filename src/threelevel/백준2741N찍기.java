dfe660be1a1fecc334154818a3dc0949d4fd890c
MJ <MJ@DESKTOP-4JANP17>
2018-11-03 13:38:38 +0900

    N찍기

---
+++ src/threelevel/백준2741N찍기.java
@@ -0,0 +1,20 @@
+package threelevel;
+
+import java.util.Scanner;
+
+public class 백준2741N찍기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		b = sc.nextInt();
+		for (a = 1; a <= b; a++) {
+			System.out.println(a);
+		}
+
+		sc.close();
+
+	}
+
+}
