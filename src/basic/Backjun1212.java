e006cea89ff6561d17cb041272a2d99dc3cd09e1
user1 <user1@DESKTOP-3R308GH>
2019-03-07 16:52:05 +0900

    a

--- src/basic/Backjun1212.java
+++ src/basic/Backjun1212.java
@@ -1,10 +1,69 @@
 package basic;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Backjun1212 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String num = br.readLine();
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<num.length();i++){
+			int n1=num.charAt(i)-'0';
+			if(i!=0){
+				if(n1==0){
+					sb.append("000");
+				}
+				else if(n1==1){
+					sb.append("001");
+				}
+				else if(n1==2){
+					sb.append("010");
+				}
+				else if(n1==3){
+					sb.append("011");
+				}
+				else if(n1==4){
+					sb.append("100");
+				}
+				else if(n1==5){
+					sb.append("101");
+				}
+				else if(n1==6){
+					sb.append("110");
+				}
+				else if(n1==7){
+					sb.append("111");
+				}
+			}
+			else{
+				 if(n1==1){
+					sb.append("1");
+				}
+				else if(n1==2){
+					sb.append("10");
+				}
+				else if(n1==3){
+					sb.append("11");
+				}
+				else if(n1==4){
+					sb.append("100");
+				}
+				else if(n1==5){
+					sb.append("101");
+				}
+				else if(n1==6){
+					sb.append("110");
+				}
+				else if(n1==7){
+					sb.append("111");
+				}
+			}
+			
+		}
+		System.out.println(sb);
+		
 	}
 
 }
